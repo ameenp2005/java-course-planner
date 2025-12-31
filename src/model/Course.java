02d0fa3a25ff511f32a88bdad88663b4e495c4d9
ameenp2005 <ameen@popal.net>
2025-12-31 17:37:03 -0500

    Create Course.java

---
+++ src/model/Course.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Course{
+
+  private String name;
+  private int credits;
+  private int difficulty;
+
+public Course(String name, int credits, int difficulty){
+  this.name = name;
+  this.credits = credits;
+  this.difficulty = difficulty;
+  
+}
+public String getName{
+  return name;
+}
+public int getCredits{
+  return credits;
+}
+  public int getDifficulty{
+  return difficulty;
+  }
+
+   
+}
